d8a22b911dbc94374f21dbdd9c4516676c3c0500
0xdk <dineshrk394@gmail.com>
2025-01-29 16:33:20 +0530

    commit repair

--- src/controllers/indexController.ts
+++ src/controllers/indexController.ts
@@ -3,22 +3,28 @@ import { Request, Response } from 'express';
 import getIdAndMolInfo from '../service/getIdAndMol';
 // Wikipedia API
 import fetchWikiInfo from '../service/wiki';
-
+import fetchElementInfoFromOpenAI from '../service/openAi';
+// helper functions
+import safetyAndToxicInfoService from '../service/safety_and_Toxic/safetyAndToxicInfoService';
+import hazardService from '../service/hazard/hazardService';
 // TS interfaces
 import { ExtractedData } from '../service/hazard/extractHazardnPictogram';
 import { IDMolResponse } from '../service/getIdAndMol';
 
-// functions to extract needed data from the response
-import extractInformation from '../service/extractHazardnPictogram';
-import extractStringsFromResponse from '../service/extractString';
-
-const data: { [key: string]: {} | null } = {};
+interface ApiResponse {
+  wikiData: string | undefined;
+  IdAndMol: Record<string, any> | string;
+  hazardsAndPictograms: ExtractedData | undefined;
+  safetyAndToxicData: {} | undefined;
+  openAiResponse: string | null | undefined;
+}
 /**
  *
  * @param req
  * @param res
  * @returns
  */
+
 async function handleApiRequest(req: Request, res: Response) {
   try {
     const userInput: string = req.body.input;
@@ -26,32 +32,38 @@ async function handleApiRequest(req: Request, res: Response) {
     // getting element summery from WIKI
     const wikiData = await fetchWikiInfo(userInput);
     // getting PubChem ID
-    const IdAndMol: IDMolResponse = await getIdAndMolInfo(userInput);
-    data['ID And Mol'] = IdAndMol.Properties;
+    const IdAndMolResponse: IDMolResponse | null = await getIdAndMolInfo(userInput);
+    const openAiResponse = await fetchElementInfoFromOpenAI(userInput);
 
-    // sending API request for all the needed headings
-    for (let heading of urlHeadings) {
-      // getting element's safety and toxic Data
-      let PharmacologyData = await getPharmacologyData(IdAndMol.Properties[0].CID, heading);
+    let IdAndMol;
+    let safetyAndToxicData;
+    let hazardsAndPictograms;
+    if (IdAndMolResponse) {
+      IdAndMol = IdAndMolResponse.Properties;
+      const pubChemID = IdAndMolResponse.Properties[0].CID;
+      // getting safety and toxicity data from PubChem DB with pubChem ID
+      safetyAndToxicData = await safetyAndToxicInfoService(pubChemID);
+      // getting hazard and pictograms from PubChem DB
 
-      if (PharmacologyData == null) {
-        //* refactor this
-        data[heading] = null;
-        continue;
-      }
-      // extracting element's safety and toxic Data
-      data[heading] = extractStringsFromResponse(PharmacologyData);
+      hazardsAndPictograms = await hazardService(pubChemID);
+    } else {
+      IdAndMol = 'No data found for this element in PubChem Data Base';
     }
-
-    // getting Hazard and Pictogram Data from PubChem DB
-    const hazardAndPictogramData = await getHazardAndPictogramData(IdAndMol.Properties[0].CID);
-    // extracting Hazard and Pictogram Dataas
-    if (hazardAndPictogramData !== null) {
-      const extractHazardAndPictogramData: ExtractedData =
-        extractInformation(hazardAndPictogramData);
-      data['Hazard And Pictogram'] = extractHazardAndPictogramData;
+    // No data found or users must entered a wrong or miss spelled name
+    if (wikiData === undefined && IdAndMol === null) {
+      res.status(404).json({
+        error:
+          'No result found for the provided input. Please check the spelling or try another name.',
+      });
+      return;
     }
-
+    const data: ApiResponse = {
+      wikiData,
+      IdAndMol,
+      hazardsAndPictograms,
+      safetyAndToxicData,
+      openAiResponse,
+    };
     res.json(data);
   } catch (error: any) {
     console.error('Error handling API requests:', error.message);
